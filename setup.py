2191d2a8af125dd820f171f23dc58a316c8e49fc
lambdalisue <lambdalisue@hashnote.net>
2018-05-22 15:31:34 +0900

    Follow PEP8 in setup.py

--- setup.py
+++ setup.py
@@ -8,29 +8,34 @@ from setuptools import setup, find_packages
 
 version = "0.1rc4"
 
+
 def read(filename):
     import os.path
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
+
 setup(
-        name="e4u",
-        version=version,
-        description = "Python's emoji conversion (unicode emoji <=> carrier's emoji) library (emoji4unicode project wrapper library)",
-        long_description=read('README.rst'),
-        classifiers = [
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python',
-            'Topic :: Internet :: WWW/HTTP',
-        ],
-        keywords = "emoji emoji4unicode mobile",
-        author = "Alisue",
-        author_email = "lambdalisue@hashnote.net",
-        url=r"https://github.com/lambdalisue/e4u",
-        download_url = r"https://github.com/lambdalisue/e4u/tarball/master",
-        license = 'BSD',
-        packages = find_packages(exclude=["test","utils"]),
-        include_package_data = True,
-        zip_safe = True,
-        install_requires=['setuptools', 'BeautifulSoup'],
+    name="e4u",
+    version=version,
+    description=(
+        "Python's emoji conversion (unicode emoji <=> carrier's emoji) "
+        "library (emoji4unicode project wrapper library)"
+    ),
+    long_description=read('README.rst'),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords="emoji emoji4unicode mobile",
+    author="Alisue",
+    author_email="lambdalisue@hashnote.net",
+    url=r"https://github.com/lambdalisue/e4u",
+    download_url=r"https://github.com/lambdalisue/e4u/tarball/master",
+    license='BSD',
+    packages=find_packages(exclude=["test", "utils"]),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=['setuptools', 'BeautifulSoup'],
 )
